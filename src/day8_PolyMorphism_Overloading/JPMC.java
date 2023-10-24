f742b79e8ff176c988ba81bd378d902beec76f11
ARC2023 <arcautomation2023@gmail.com>
2023-10-24 08:09:20 +0530

    Getter setter for PULL CHANGE

--- src/day8_PolyMorphism_Overloading/JPMC.java
+++ src/day8_PolyMorphism_Overloading/JPMC.java
@@ -4,6 +4,18 @@ public class JPMC {
 
 	int hj=9;
 	int ho=90;
+	public int getHj() {
+		return hj;
+	}
+	public void setHj(int hj) {
+		this.hj = hj;
+	}
+	public int getHo() {
+		return ho;
+	}
+	public void setHo(int ho) {
+		this.ho = ho;
+	}
 	
 	
 	
