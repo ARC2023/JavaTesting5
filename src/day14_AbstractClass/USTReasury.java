fa7d88c63ab1e0fefb315fee8e4f6de395537e49
ARC2023 <arcautomation2023@gmail.com>
2023-11-06 08:21:56 +0530

    Abstract Class

---
+++ src/day14_AbstractClass/USTReasury.java
@@ -0,0 +1,24 @@
+package day14_AbstractClass;
+
+public abstract class USTReasury {
+
+	
+	
+	
+	public void IMPS() {
+		System.out.println("Has Implementation");
+	}
+
+	public void UPI() {
+		System.out.println("Has Implementation");
+	}
+
+	public void RTGS() {
+		System.out.println("Has Implementation");
+	}
+
+	public void NEFT() {
+		System.out.println("Has Implementation");
+	}
+
+}
